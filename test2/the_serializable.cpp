56c2aa7f253414cac35d6dde4c3353f3c363ea8e
Francois Mauger <mauger@lpccaen.in2p3.fr>
2019-04-28 08:31:33 +0200

    Add test2c program

---
+++ test2/the_serializable.cpp
@@ -0,0 +1,12 @@
+
+// Boost:
+#include <boost/serialization/export.hpp>
+
+// This project:
+#include <class4-serial.hpp>
+#include <lib1/archives_instantiation.hpp>
+
+LIB1_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(class4)
+BOOST_CLASS_EXPORT_IMPLEMENT(class4)
+
+// end
